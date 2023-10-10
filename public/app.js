dd8db4751ee717ab572f2f603ab531d49b2553a3
Chris <117781333+chrism-la@users.noreply.github.com>
2023-10-09 20:12:04 -0700

    created first Ad

--- public/app.js
+++ public/app.js
@@ -1,20 +1,32 @@
 // get user's data
 // get user's coordinates
 
-
 // get user's time
-
+function getUserTime() {
+    const currentTime = new Date();
+    return currentTime.getHours();
+}
 
 // helper functions
 // check time of day
-
-
+function getMeal() {
+    const currentHour = getUserTime();
+    if (currentHour > 0 && currentHour <= 11) return "breakfast";
+    else if (currentHour > 11 && currentHour <= 16) return "lunch";
+    else if (currentHour > 16 && currentHour <= 20) return "dinner";
+    else return "late night snack";
+}
 // build ads
 // build ad 1
-
-
+function createAd1() {
+    const meal = getMeal();
+    let ad1Div = document.querySelector(".ad1");
+    let adText = document.createElement("p");
+    adText.innerHTML = `We have <strong>the best ${meal}</strong> in town. Come and try us!`;
+    ad1Div.append(adText);
+}
+createAd1();
 // build ad 2
-
 
 // event listeners
 // on load, build ads
