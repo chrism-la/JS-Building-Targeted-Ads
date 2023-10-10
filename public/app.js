13abd6c8d15f01efa3bf74b2fe7ac52185971e33
Chris <117781333+chrism-la@users.noreply.github.com>
2023-10-09 20:34:32 -0700

    added windowOnLoad

--- public/app.js
+++ public/app.js
@@ -1,6 +1,11 @@
 // get user's data
 // get user's coordinates
-
+async function getUserCoordinates() {
+    let position = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+    return [position.coords.latitude, position.coords.longitude];
+}
 // get user's time
 function getUserTime() {
     const currentTime = new Date();
@@ -25,8 +30,18 @@ function createAd1() {
     adText.innerHTML = `We have <strong>the best ${meal}</strong> in town. Come and try us!`;
     ad1Div.append(adText);
 }
-createAd1();
 // build ad 2
-
+async function createAd2() {
+    const userCoords = getUserCoordinates();
+    let ad2Div = document.querySelector(".ad2");
+    let adText = document.createElement("p");
+    const href = `https://www.google.com/maps/search/coffee/@${userCoords[0]},${userCoords[1]},14z/data=!3m1!4b1?entry=ttu`;
+    adText.innerHTML = `It is time for a cup of coffee.<a href=${href}> Here are some awesome coffee shops</a>. Enjoy!`;
+    ad2Div.append(adText);
+}
 // event listeners
 // on load, build ads
+window.onload = () => {
+    createAd1();
+    createAd2();
+};
